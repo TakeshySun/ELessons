10dab7fa227b26cf85fa387546bb74c1f80ddd13
Oleksandr_Eskin <oleksandr.eskin@mckesson.com>
2020-12-11 13:44:10 +0200

    regEx ip + bank

--- src/main/java/Regular/RegularPattern.java
+++ src/main/java/Regular/RegularPattern.java
@@ -18,28 +18,12 @@ import java.util.regex.Pattern;
  */
 public class RegularPattern {
     public static void main(String[] args) {
-        //Email List
-        List<String> emails = new ArrayList<>();
-        emails.add("one@domain.com");
-        emails.add("two@domain.co.in");
-        emails.add("user1@domain.com");
-        emails.add("user2.name@domain.com");
-        emails.add("user#@domain.co.in");
-        //Invalid emails
-        emails.add("user@domaincom");
-        emails.add("fail#domain.com");
-        emails.add("@gmail.com");
-        emails.add("ff,ff@.");
-        emails.add(" @ . ");
-        emails.add(" @k. ");
-        emails.add("@k. ");
-        //Pattern
-        String regex = "^(.+\\S)@(.+\\S)\\.(.+\\S)$";
-        //Method
-        System.out.println(getCorrectEmails(emails, regex));
+        System.out.println(listEmails());
+        System.out.println(listBankCard());
+        System.out.println(listIp());
     }
 
-    public static List<String> getCorrectEmails(List<String> emails, String regEx){
+    public static List<String> getCorrectValues(List<String> emails, String regEx){
         Pattern pattern = Pattern.compile(regEx);
         List<String> matchesValue = new ArrayList<>();
         for(String email : emails){
@@ -49,8 +33,66 @@ public class RegularPattern {
             }
         }
         return matchesValue;
-
     }
 
+    public static List<String> listEmails(){
+        //Email List
+        List<String> emails = new ArrayList<>();
+        emails.add("one@domain.com");
+        emails.add("two@domain.co.in");
+        emails.add("user1@domain.com");
+        emails.add("user2.name@domain.com");
+        emails.add("user#@domain.co.in");
+        emails.add("sd_sd@d_d.com");
+        //Invalid emails
+        emails.add("user@domaincom");
+        emails.add("fail#domain.com");
+        emails.add("@gmail.com");
+        emails.add("ff,ff@.");
+        emails.add(" @ . ");
+        emails.add(" @k. ");
+        emails.add("@k.");
+        emails.add("^@::.com");
+        //Pattern
+        String regex = "^(.+\\S)@(\\w+)\\.(.+\\S)$";
+        //Method
+        return getCorrectValues(emails, regex);
+    }
 
+    public static List<String> listBankCard(){
+        List<String> cards = new ArrayList<>();
+        cards.add("1456-4444-5555-6666");
+        cards.add("145-4444-5555-6666");
+        cards.add("1456-444-5555-6666");
+        cards.add("1456-4444-555-6666");
+        cards.add("1456-4444-5555-666");
+        cards.add("aaaa-dddd-dddd-dddd");
+        cards.add("4444 4444 4444 4444");
+        cards.add("4444444444444444");
+        cards.add("abfd444444444444");
+        cards.add("aaaa bbbb bbbb bbbb");
+
+        //Pattern
+        String regex = "^(\\d{4}).?(\\d{4}).?(\\d{4}).?(\\d{4})$";
+        return getCorrectValues(cards, regex);
+    }
+
+    public static List<String> listIp(){
+        List<String> listIp = new ArrayList<>();
+        listIp.add("115.42.150.37");
+        listIp.add("192.168.0.1");
+        listIp.add("110.234.52.124");
+        //Ivalid
+        listIp.add("210.110");
+        listIp.add("255");
+        listIp.add("y.y.y.y");
+        listIp.add("255.0.0.y");
+        listIp.add("666.10.10.20");
+        listIp.add("4444.11.11.11");
+        listIp.add("33.3333.33.3");
+        //Pattern
+        String regex = String.format("^%1$s\\.%1$s\\.%1$s\\.%1$s$", "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)");
+
+        return getCorrectValues(listIp, regex);
+    }
 }
